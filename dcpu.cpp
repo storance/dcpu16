097f0a89acb5701e1f0d98565108ca8fc62a77fc
Steven Torance <storance@gmail.com>
2012-04-08 16:42:59 -0400

    Missed file

--- dcpu.cpp
+++ dcpu.cpp
@@ -1,102 +1,18 @@
 #include <inttypes.h>
-#include <assert.h>
-#include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
+#include <cstring>
+#include <cerrno>
 #include <exception>
 #include <stdexcept>
 #include <iomanip>
 
-#include "dcpu.h"
+#include <boost/format.hpp>
+
+#include "dcpu.hpp"
 
 using namespace std;
-
-static word_t literals[] = {
-	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,
-	0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13,
-	0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d,
-	0x1e, 0x1f
-};
-
-void OpcodeArgument::resolve(DCPU &cpu) {
-	switch (type) {
-	case arg_a: case arg_b: case arg_c: case arg_x:
-	case arg_y: case arg_z: case arg_i: case arg_j:
-		value = cpu.getRegisterPtr(type);
-		break;
-	case arg_ptr_a: case arg_ptr_b: case arg_ptr_c: case arg_ptr_x:
-	case arg_ptr_y: case arg_ptr_z: case arg_ptr_i: case arg_ptr_j:
-		{
-			uint8_t regType = type - arg_ptr_a;
-			word_t regValue = cpu.getRegisterValue(regType);
-
-			value = cpu.getMemoryPtr(regValue);
-		}
-		break;
-	case arg_ptr_offset_a: case arg_ptr_offset_b: case arg_ptr_offset_c:
-	case arg_ptr_offset_x: case arg_ptr_offset_y: case arg_ptr_offset_z:
-	case arg_ptr_offset_i: case arg_ptr_offset_j:
-		{
-			uint8_t regType = type - arg_ptr_offset_a;
-			word_t regValue = cpu.getRegisterValue(regType);
-			word_t nextWord = cpu.getNextWord();
-
-			value = cpu.getMemoryPtr(regValue + nextWord);
-		}
-		break;
-	case arg_pop:
-		value = cpu.popStack();
-		break;
-	case arg_peek:
-		value = cpu.peekStack();
-		break;
-	case arg_push:
-		value = cpu.pushStack();
-		break;
-	case arg_sp:
-		value = &cpu.sp;
-		break;
-	case arg_pc:
-		value = &cpu.pc;
-		break;
-	case arg_o:
-		value = &cpu.o;
-		break;
-	case arg_ptr_next_word:
-		{
-			word_t nextWord = cpu.getNextWord();
-			value = cpu.getMemoryPtr(nextWord);
-		}
-		break;
-	case arg_next_word:
-		value = cpu.getNextWordPtr();
-		break;
-	default:
-		if (type > 0x3f) {
-			throw logic_error("Invalid argument literal");
-		}
-		value = &literals[type - arg_literal];
-		break;
-	}
-}
-
-bool OpcodeArgument::isNextWordUsed() const {
-	return (type >= arg_ptr_offset_a && type <= arg_ptr_offset_j)
-		|| type == arg_ptr_next_word || type == arg_next_word;
-}
-
-Opcode::Opcode(word_t instruction) {
-
-	opcode = (OpcodeType)(instruction & 0x000f);
-	a.setType((instruction >> 4) & 0x003f);
-	b.setType((instruction >> 10) & 0x003f);
-
-	if (opcode == op_non_basic) {
-		size = 1 + b.isNextWordUsed();
-	} else {
-		size = 1 + a.isNextWordUsed() + b.isNextWordUsed();
-	}
-}
 
 DCPU::DCPU() {
 
@@ -193,25 +109,36 @@ size_t DCPU::load(const char *filename) {
 	
 	file.open(filename);
 	if (!file) {
-		throw runtime_error("Failed to load program");
+		throw runtime_error(str(boost::format("Failed to open the file %s: %s") 
+				% filename % strerror(errno)));
 	}
 
-	file.read((char*)memory, TOTAL_MEMORY * sizeof(word_t));
-	size_t size =  file.gcount() / 2;
+	size_t index = 0;
+	while (file) {
+		uint8_t b1 = file.get();
+		uint8_t b2 = file.get();
+		if (file.bad()) {
+			throw runtime_error(str(boost::format("Failed to read the next "
+				"word from the file %s: %s") % filename 
+				% strerror(errno)));
+		}
+
+		memory[index++] = (b1 << 8) | b2;
+	}
 	file.close();
 
-	return size;
+	return index;
 }
 
 void DCPU::execute(OpcodeHandler* handler) {
 	while (!halted) {
-		Opcode opcode(getNextWord());
+		Opcode opcode(getNextWord(), pc-1);
 		handler->execute(*this, opcode);
 	}
 }
 
 void DCPU::skipNext() {
-	Opcode opcode(getMemoryValue(pc));
+	Opcode opcode(getMemoryValue(pc), pc);
 
 	pc += opcode.getSize();
 	addCycles(1);
