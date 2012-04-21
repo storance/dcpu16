3e11e31c639cd1c43de46a3afab261edbb6dd9a8
Steven Torance <storance@gmail.com>
2012-04-21 15:39:52 -0400

    Missed makefile and tests

--- assembler/test/LexerTest.cpp
+++ assembler/test/LexerTest.cpp
@@ -9,10 +9,10 @@ using namespace dcpu;
 using namespace dcpu::lexer;
 
 typedef string::const_iterator iterator_type;
-typedef Lexer<iterator_type, std::vector<token_type>> lexer_type;
+typedef Lexer<iterator_type, std::vector<shared_ptr<Token>>> lexer_type;
 
 #define ASSERT_LOCATION(token, lineNum, columnNum) { \
-	token_type t = token; \
+	shared_ptr<Token> t = token; \
 	EXPECT_EQ("<LexerTest>", t->location.sourceName); \
 	EXPECT_EQ(lineNum, t->location.line); \
 	EXPECT_EQ(columnNum, t->location.column); \
@@ -34,7 +34,7 @@ typedef Lexer<iterator_type, std::vector<token_type>> lexer_type;
 	EXPECT_EQ(expectedBase, integerToken->base); \
 }
 
-void runParser(string input, size_t expectedTokens, vector<token_type> &tokens) {
+void runParser(string input, size_t expectedTokens, vector<shared_ptr<Token>> &tokens) {
 	lexer_type lexer(input.begin(), input.end(), "<LexerTest>");
     lexer.parse();
 
@@ -44,7 +44,7 @@ void runParser(string input, size_t expectedTokens, vector<token_type> &tokens) 
 }
 
 TEST(LexerTest, IdentifierStartsUnderscore) {
-    vector<token_type> tokens;
+    vector<shared_ptr<Token>> tokens;
 
     ASSERT_NO_FATAL_FAILURE(runParser("_a1_?.$#@", 2, tokens));
 
@@ -55,7 +55,7 @@ TEST(LexerTest, IdentifierStartsUnderscore) {
 }
 
 TEST(LexerTest, IdentifierStartsPeriod) {
-	vector<token_type> tokens;
+	vector<shared_ptr<Token>> tokens;
     ASSERT_NO_FATAL_FAILURE(runParser(".aaa111", 2, tokens));
 
 	EXPECT_TRUE(tokens[0]->isIdentifier());
@@ -65,7 +65,7 @@ TEST(LexerTest, IdentifierStartsPeriod) {
 }
 
 TEST(LexerTest, IdentifierStartsQuestion) {
-	vector<token_type> tokens;
+	vector<shared_ptr<Token>> tokens;
     ASSERT_NO_FATAL_FAILURE(runParser("?aaa111", 2, tokens));
 
     EXPECT_TRUE(tokens[0]->isIdentifier());
@@ -75,7 +75,7 @@ TEST(LexerTest, IdentifierStartsQuestion) {
 }
 
 TEST(LexerTest, IdentifierStartsLetter) {
-	vector<token_type> tokens;
+	vector<shared_ptr<Token>> tokens;
     ASSERT_NO_FATAL_FAILURE(runParser("aaa111", 2, tokens));
 
     EXPECT_TRUE(tokens[0]->isIdentifier());
@@ -85,7 +85,7 @@ TEST(LexerTest, IdentifierStartsLetter) {
 }
 
 TEST(LexerTest, DecimalNumber) {
-    vector<token_type> tokens;
+    vector<shared_ptr<Token>> tokens;
 
     ASSERT_NO_FATAL_FAILURE(runParser("100", 2, tokens));
 
@@ -96,7 +96,7 @@ TEST(LexerTest, DecimalNumber) {
 }
 
 TEST(LexerTest, HexNumberLowercase) {
-    vector<token_type> tokens;
+    vector<shared_ptr<Token>> tokens;
     ASSERT_NO_FATAL_FAILURE(runParser("0xff", 2, tokens));
 
 	ASSERT_TRUE(tokens[0]->isInteger());
@@ -106,7 +106,7 @@ TEST(LexerTest, HexNumberLowercase) {
 }
 
 TEST(LexerTest, HexNumberUppercase) {
-	vector<token_type> tokens;
+	vector<shared_ptr<Token>> tokens;
     ASSERT_NO_FATAL_FAILURE(runParser("0X1D", 2, tokens));
 
     ASSERT_TRUE(tokens[0]->isInteger());
@@ -116,7 +116,7 @@ TEST(LexerTest, HexNumberUppercase) {
 }
 
 TEST(LexerTest, BinaryNumberLowercase) {
-    vector<token_type> tokens;
+    vector<shared_ptr<Token>> tokens;
 
     ASSERT_NO_FATAL_FAILURE(runParser("0b1011", 2, tokens));
 
@@ -127,7 +127,7 @@ TEST(LexerTest, BinaryNumberLowercase) {
 }
 
 TEST(LexerTest, BinaryNumberUppercase) {
-	vector<token_type> tokens;
+	vector<shared_ptr<Token>> tokens;
     ASSERT_NO_FATAL_FAILURE(runParser("0B10001011", 2, tokens));
 
     ASSERT_TRUE(tokens[0]->isInteger());
@@ -137,7 +137,7 @@ TEST(LexerTest, BinaryNumberUppercase) {
 }
 
 TEST(LexerTest, OctalNumberLowercase) {
-    vector<token_type> tokens;
+    vector<shared_ptr<Token>> tokens;
     ASSERT_NO_FATAL_FAILURE(runParser("0o32", 2, tokens));
 
     ASSERT_TRUE(tokens[0]->isInteger());
@@ -147,7 +147,7 @@ TEST(LexerTest, OctalNumberLowercase) {
 }
 
 TEST(LexerTest, OctalNumberUppercase) {
-	vector<token_type> tokens;
+	vector<shared_ptr<Token>> tokens;
     ASSERT_NO_FATAL_FAILURE(runParser("0O27", 2, tokens));
 
     ASSERT_TRUE(tokens[0]->isInteger());
@@ -157,7 +157,7 @@ TEST(LexerTest, OctalNumberUppercase) {
 }
 
 TEST(LexerTest, InvalidDecimalNumber) {
-    vector<token_type> tokens;
+    vector<shared_ptr<Token>> tokens;
     ASSERT_NO_FATAL_FAILURE(runParser("100a3", 2, tokens));
 
     ASSERT_TRUE(tokens[0]->isInvalidInteger());
@@ -167,7 +167,7 @@ TEST(LexerTest, InvalidDecimalNumber) {
 }
 
 TEST(LexerTest, InvalidHexNumber) {
-	vector<token_type> tokens;
+	vector<shared_ptr<Token>> tokens;
     ASSERT_NO_FATAL_FAILURE(runParser("0X100Z3", 2, tokens));
 
     ASSERT_TRUE(tokens[0]->isInvalidInteger());
@@ -177,7 +177,7 @@ TEST(LexerTest, InvalidHexNumber) {
 }
 
 TEST(LexerTest, OctalWithInvalidNumber) {
-	vector<token_type> tokens;
+	vector<shared_ptr<Token>> tokens;
     ASSERT_NO_FATAL_FAILURE(runParser("0o10093", 2, tokens));
 
     ASSERT_TRUE(tokens[0]->isInvalidInteger());
@@ -187,7 +187,7 @@ TEST(LexerTest, OctalWithInvalidNumber) {
 }
 
 TEST(LexerTest, OctalWithInvalidLetter) {
-	vector<token_type> tokens;
+	vector<shared_ptr<Token>> tokens;
     ASSERT_NO_FATAL_FAILURE(runParser("0o100a3", 2, tokens));
 
     ASSERT_TRUE(tokens[0]->isInvalidInteger());
@@ -197,7 +197,7 @@ TEST(LexerTest, OctalWithInvalidLetter) {
 }
 
 TEST(LexerTest, BinaryWithInvalidNumber) {
-	vector<token_type> tokens;
+	vector<shared_ptr<Token>> tokens;
     ASSERT_NO_FATAL_FAILURE(runParser("0b1113", 2, tokens));
 
     ASSERT_TRUE(tokens[0]->isInvalidInteger());
@@ -207,7 +207,7 @@ TEST(LexerTest, BinaryWithInvalidNumber) {
 }
 
 TEST(LexerTest, BinaryWithInvalidLetter) {
-	vector<token_type> tokens;
+	vector<shared_ptr<Token>> tokens;
     ASSERT_NO_FATAL_FAILURE(runParser("0B111a", 2, tokens));
 
     ASSERT_TRUE(tokens[0]->isInvalidInteger());
@@ -217,7 +217,7 @@ TEST(LexerTest, BinaryWithInvalidLetter) {
 }
 
 TEST(LexerTest, OverflowNumber) {
-    vector<token_type> tokens;
+    vector<shared_ptr<Token>> tokens;
     ASSERT_NO_FATAL_FAILURE(runParser("4294967296", 2, tokens));
 
     ASSERT_TRUE(tokens[0]->isInteger());
@@ -227,7 +227,7 @@ TEST(LexerTest, OverflowNumber) {
 }
 
 TEST(LexerTest, DecimalNumberAtUint32Max) {
-    vector<token_type> tokens;
+    vector<shared_ptr<Token>> tokens;
     ASSERT_NO_FATAL_FAILURE(runParser("4294967295", 2, tokens));
 
     ASSERT_TRUE(tokens[0]->isInteger());
@@ -237,7 +237,7 @@ TEST(LexerTest, DecimalNumberAtUint32Max) {
 }
 
 TEST(LexerTest, Increment) {
-    vector<token_type> tokens;
+    vector<shared_ptr<Token>> tokens;
     ASSERT_NO_FATAL_FAILURE(runParser("++", 2, tokens));
 
 	EXPECT_TRUE(tokens[0]->isIncrement());
@@ -247,7 +247,7 @@ TEST(LexerTest, Increment) {
 }
 
 TEST(LexerTest, Decrement) {
-	vector<token_type> tokens;
+	vector<shared_ptr<Token>> tokens;
     ASSERT_NO_FATAL_FAILURE(runParser("--", 2, tokens));
 
     EXPECT_TRUE(tokens[0]->isDecrement());
@@ -257,7 +257,7 @@ TEST(LexerTest, Decrement) {
 }
 
 TEST(LexerTest, ShiftLeft) {
-	vector<token_type> tokens;
+	vector<shared_ptr<Token>> tokens;
     ASSERT_NO_FATAL_FAILURE(runParser("<<", 2, tokens));
 
     EXPECT_TRUE(tokens[0]->isShiftLeft());
@@ -267,7 +267,7 @@ TEST(LexerTest, ShiftLeft) {
 }
 
 TEST(LexerTest, ShiftRight) {
-	vector<token_type> tokens;
+	vector<shared_ptr<Token>> tokens;
     ASSERT_NO_FATAL_FAILURE(runParser(">>", 2, tokens));
 
     EXPECT_TRUE(tokens[0]->isShiftRight());
@@ -277,7 +277,7 @@ TEST(LexerTest, ShiftRight) {
 }
 
 TEST(LexerTest, Newline) {
-	vector<token_type> tokens;
+	vector<shared_ptr<Token>> tokens;
 	ASSERT_NO_FATAL_FAILURE(runParser("\n", 2, tokens));
 
     EXPECT_TRUE(tokens[0]->isNewline());
@@ -285,7 +285,7 @@ TEST(LexerTest, Newline) {
 }
 
 TEST(LexerTest, SingleCharacters) {
-	vector<token_type> tokens;
+	vector<shared_ptr<Token>> tokens;
     ASSERT_NO_FATAL_FAILURE(runParser("@", 2, tokens));
 
     EXPECT_TRUE(tokens[0]->isCharacter('@'));
@@ -332,58 +332,19 @@ TEST(LexerTest, SingleCharacters) {
     EXPECT_TRUE(tokens[1]->isEOI());
 }
 
-TEST(LexerTest, Whitespace) {
-	vector<token_type> tokens;
-	ASSERT_NO_FATAL_FAILURE(runParser(" \t1 + 3\f   ", 8, tokens));
-
-	int index = 0;
-	EXPECT_TRUE(tokens[index++]->isWhitespace());
-
-	ASSERT_TRUE(tokens[index]->isInteger());
-	EXPECT_INTEGER(tokens[index++], 1, false);
-
-	EXPECT_TRUE(tokens[index++]->isWhitespace());
-
-	EXPECT_TRUE(tokens[index++]->isCharacter('+'));
-
-	EXPECT_TRUE(tokens[index++]->isWhitespace());
-
-	ASSERT_TRUE(tokens[index]->isInteger());
-	EXPECT_INTEGER(tokens[index++], 3, false);
-
-	EXPECT_TRUE(tokens[index++]->isWhitespace());
-
-	EXPECT_TRUE(tokens[index++]->isEOI());
-}
-
-TEST(LexerTest, Comment) {
-	vector<token_type> tokens;
-	ASSERT_NO_FATAL_FAILURE(runParser("; test comment\n", 3, tokens));
-
-	EXPECT_TRUE(tokens[0]->isComment());
-	EXPECT_EQ(tokens[0]->content, "; test comment");
-
-	EXPECT_TRUE(tokens[1]->isNewline());
-	EXPECT_TRUE(tokens[2]->isEOI());
-}
-
 TEST(LexerTest, SimpleExpression) {
-	vector<token_type> tokens;
-	ASSERT_NO_FATAL_FAILURE(runParser("set A, b\nset [J], 0x400\nlabel: JSR label+4\n", 26, tokens));
+	vector<shared_ptr<Token>> tokens;
+	ASSERT_NO_FATAL_FAILURE(runParser("set A, b\nset [J], 0x400\nlabel: JSR label+4\n", 20, tokens));
 
 	int index = 0;
 	// Line 1
 	EXPECT_TRUE(tokens[index]->isIdentifier());
 	EXPECT_EQ("set", tokens[index++]->content);
 
-	EXPECT_TRUE(tokens[index++]->isWhitespace());
-
 	EXPECT_TRUE(tokens[index]->isIdentifier());
 	EXPECT_EQ("A", tokens[index++]->content);
 
 	EXPECT_TRUE(tokens[index++]->isCharacter(','));
-
-	EXPECT_TRUE(tokens[index++]->isWhitespace());
 
 	EXPECT_TRUE(tokens[index]->isIdentifier());
 	EXPECT_EQ("b", tokens[index++]->content);
@@ -394,8 +355,6 @@ TEST(LexerTest, SimpleExpression) {
 	EXPECT_TRUE(tokens[index]->isIdentifier());
 	EXPECT_EQ("set", tokens[index++]->content);
 
-	EXPECT_TRUE(tokens[index++]->isWhitespace());
-
 	EXPECT_TRUE(tokens[index++]->isCharacter('['));
 
 	EXPECT_TRUE(tokens[index]->isIdentifier());
@@ -404,8 +363,6 @@ TEST(LexerTest, SimpleExpression) {
 	EXPECT_TRUE(tokens[index++]->isCharacter(']'));
 
 	EXPECT_TRUE(tokens[index++]->isCharacter(','));
-
-	EXPECT_TRUE(tokens[index++]->isWhitespace());
 
 	ASSERT_TRUE(tokens[index]->isInteger());
 	EXPECT_INTEGER(tokens[index++], 0x400, false);
@@ -418,12 +375,8 @@ TEST(LexerTest, SimpleExpression) {
 
 	EXPECT_TRUE(tokens[index++]->isCharacter(':'));
 
-	EXPECT_TRUE(tokens[index++]->isWhitespace());
-
 	EXPECT_TRUE(tokens[index]->isIdentifier());
 	EXPECT_EQ("JSR", tokens[index++]->content);
-
-	EXPECT_TRUE(tokens[index++]->isWhitespace());
 
 	EXPECT_TRUE(tokens[index]->isIdentifier());
 	EXPECT_EQ("label", tokens[index++]->content);
@@ -439,8 +392,8 @@ TEST(LexerTest, SimpleExpression) {
 }
 
 TEST(LexerTest, Location) {
-	vector<token_type> tokens;
-	ASSERT_NO_FATAL_FAILURE(runParser("set A, b\n  set [J], 0x400\n;a test comment\nSET I, 1\n", 27, tokens));
+	vector<shared_ptr<Token>> tokens;
+	ASSERT_NO_FATAL_FAILURE(runParser("set A, b\n  set [J], 0x400\n;a test comment\nSET I, 1\n", 19, tokens));
 
 	int index = 0;
 
@@ -449,9 +402,6 @@ TEST(LexerTest, Location) {
 	EXPECT_EQ("set", tokens[index]->content);
 	ASSERT_LOCATION(tokens[index++], 1, 1);
 
-	EXPECT_TRUE(tokens[index]->isWhitespace());
-	ASSERT_LOCATION(tokens[index++], 1, 4);
-
 	EXPECT_TRUE(tokens[index]->isIdentifier());
 	EXPECT_EQ("A", tokens[index]->content);
 	ASSERT_LOCATION(tokens[index++], 1, 5);
@@ -459,8 +409,6 @@ TEST(LexerTest, Location) {
 	EXPECT_TRUE(tokens[index]->isCharacter(','));
 	ASSERT_LOCATION(tokens[index++], 1, 6);
 
-	EXPECT_TRUE(tokens[index]->isWhitespace());
-	ASSERT_LOCATION(tokens[index++], 1, 7);
 
 	EXPECT_TRUE(tokens[index]->isIdentifier());
 	EXPECT_EQ("b", tokens[index]->content);
@@ -470,15 +418,9 @@ TEST(LexerTest, Location) {
 	ASSERT_LOCATION(tokens[index++], 1, 9);
 
 	// Line 2
-	EXPECT_TRUE(tokens[index]->isWhitespace());
-	ASSERT_LOCATION(tokens[index++], 2, 1);
-
 	EXPECT_TRUE(tokens[index]->isIdentifier());
 	EXPECT_EQ("set", tokens[index]->content);
 	ASSERT_LOCATION(tokens[index++], 2, 3);
-
-	EXPECT_TRUE(tokens[index]->isWhitespace());
-	ASSERT_LOCATION(tokens[index++], 2, 6);
 
 	EXPECT_TRUE(tokens[index]->isCharacter('['));
 	ASSERT_LOCATION(tokens[index++], 2, 7);
@@ -493,9 +435,6 @@ TEST(LexerTest, Location) {
 	EXPECT_TRUE(tokens[index]->isCharacter(','));
 	ASSERT_LOCATION(tokens[index++], 2, 10);
 
-	EXPECT_TRUE(tokens[index]->isWhitespace());
-	ASSERT_LOCATION(tokens[index++], 2, 11);
-
 	ASSERT_TRUE(tokens[index]->isInteger());
 	EXPECT_INTEGER(tokens[index], 0x400, false);
 	ASSERT_LOCATION(tokens[index++], 2, 12);
@@ -504,10 +443,6 @@ TEST(LexerTest, Location) {
 	ASSERT_LOCATION(tokens[index++], 2, 17);
 
 	// Line 3
-	EXPECT_TRUE(tokens[index]->isComment());
-	EXPECT_EQ(";a test comment", tokens[index]->content);
-	ASSERT_LOCATION(tokens[index++], 3, 1);
-
 	EXPECT_TRUE(tokens[index]->isNewline());
 	ASSERT_LOCATION(tokens[index++], 3, 16);
 
@@ -516,18 +451,12 @@ TEST(LexerTest, Location) {
 	EXPECT_EQ("SET", tokens[index]->content);
 	ASSERT_LOCATION(tokens[index++], 4, 1);
 
-	EXPECT_TRUE(tokens[index]->isWhitespace());
-	ASSERT_LOCATION(tokens[index++], 4, 4);
-
 	EXPECT_TRUE(tokens[index]->isIdentifier());
 	EXPECT_EQ("I", tokens[index]->content);
 	ASSERT_LOCATION(tokens[index++], 4, 5);
 
 	EXPECT_TRUE(tokens[index]->isCharacter(','));
 	ASSERT_LOCATION(tokens[index++], 4, 6);
-
-	EXPECT_TRUE(tokens[index]->isWhitespace());
-	ASSERT_LOCATION(tokens[index++], 4, 7);
 
 	ASSERT_TRUE(tokens[index]->isInteger());
 	EXPECT_INTEGER(tokens[index], 1, false);
--- assembler/test/ParserTest.cpp
+++ assembler/test/ParserTest.cpp
@@ -2,55 +2,70 @@
 
 #include <iostream>
 #include <list>
+#include <functional>
 #include <gtest/gtest.h>
 
-#include <boost/bind.hpp>
-
 using namespace std;
+using namespace std::placeholders;
 using namespace dcpu;
+using namespace dcpu::common;
 using namespace dcpu::ast;
 using namespace dcpu::parser;
 using namespace dcpu::lexer;
 
-void verifyLabel(list<Statement>::const_iterator &it, string expectedName) {
-	Statement statement = *it++;
+typedef list<shared_ptr<Statement>>::iterator StatementIterator;
 
-	Label *label = boost::get<Label>(&statement);
-	ASSERT_TRUE(label != nullptr);
-	EXPECT_EQ(expectedName, label->name);
+void verifyLabel(StatementIterator &it, const string &expectedName) {
+	shared_ptr<ast::Statement> statement = *it++;
+
+	shared_ptr<ast::Label> label = dynamic_pointer_cast<ast::Label>(statement);
+	ASSERT_TRUE((bool)label);
+	EXPECT_EQ(expectedName, label->_name);
 }
 
 template <typename FunctionA, typename FunctionB>
-void verifyInstruction(list<Statement>::iterator &it, Opcode opcode, FunctionA verifyA, FunctionB verifyB) {
-	Statement statement = *it++;
+void verifyInstruction(StatementIterator &it, Opcode opcode, FunctionA verifyA, FunctionB verifyB) {
+	shared_ptr<ast::Statement> statement = *it++;
 
-	Instruction *instruction = boost::get<Instruction>(&statement);
-	ASSERT_TRUE(instruction != nullptr);
-	EXPECT_EQ(opcode, instruction->opcode);
+	shared_ptr<ast::Instruction> instruction = dynamic_pointer_cast<ast::Instruction>(statement);
+	ASSERT_TRUE((bool)instruction);
+	EXPECT_EQ(opcode, instruction->_opcode);
 
 	{
 		SCOPED_TRACE("First Argument"); 
-		verifyA(instruction->a);
+		verifyA(instruction->_a);
 	}
 	{
 		SCOPED_TRACE("Second Argument"); 
-		verifyB(instruction->b);
+		verifyB(instruction->_b);
 	}
 }
 
-void verifyArgumentIsRegister(Register expectedRegister, Argument arg) {
-	Register *type = boost::get<Register>(&arg);
-	ASSERT_TRUE(type != nullptr);
-	EXPECT_EQ(expectedRegister, *type);
+template <typename Function>
+void verifyArgumentIsExpression(shared_ptr<Argument> arg, Function verifyArg) {
+	shared_ptr<ExpressionArgument> exprArg = dynamic_pointer_cast<ExpressionArgument>(arg);
+
+	ASSERT_TRUE((bool)exprArg);
+	{
+		SCOPED_TRACE("Verify Expression Argument");
+		verifyArg(exprArg->_expr);
+	}
 }
 
-void verifyArgumentIsNull(Argument arg) {
-	EXPECT_EQ(0, arg.which());
+void verifyExpressionIsRegister(shared_ptr<Expression> expr, common::Register expectedRegister) {
+	shared_ptr<RegisterOperand> registerOp = dynamic_pointer_cast<RegisterOperand>(expr);
+
+	ASSERT_TRUE((bool)registerOp);
+	EXPECT_EQ(expectedRegister, registerOp->_register);
+}
+
+void verifyArgumentIsNull(shared_ptr<Argument> arg) {
+	EXPECT_FALSE(arg);
 }
 
 typedef string::const_iterator lexer_iterator;
 
-void runParser(string content, int expectedStatements, std::list<Statement> &statements) {
+void runParser(string content, int expectedStatements, list<shared_ptr<Statement>> &statements) {
 	Lexer<lexer_iterator> lexer(content.begin(), content.end(), "<ParserTest>");
 	lexer.parse();
 
@@ -59,12 +74,25 @@ void runParser(string content, int expectedStatements, std::list<Statement> &sta
 	Parser parser(lexer.tokens.begin(), lexer.tokens.end(), errorHandler);
 	parser.parse();	
 
-	statements = parser.statements;
+	statements = parser._statements;
 	ASSERT_EQ(expectedStatements, statements.size());
 }
 
+auto isRegisterA  = bind(&verifyExpressionIsRegister, _1, Register::A);
+auto isRegisterB  = bind(&verifyExpressionIsRegister, _1, Register::B);
+auto isRegisterC  = bind(&verifyExpressionIsRegister, _1, Register::C);
+auto isRegisterX  = bind(&verifyExpressionIsRegister, _1, Register::X);
+auto isRegisterY  = bind(&verifyExpressionIsRegister, _1, Register::Y);
+auto isRegisterZ  = bind(&verifyExpressionIsRegister, _1, Register::Z);
+auto isRegisterI  = bind(&verifyExpressionIsRegister, _1, Register::I);
+auto isRegisterJ  = bind(&verifyExpressionIsRegister, _1, Register::J);
+auto isRegisterPC = bind(&verifyExpressionIsRegister, _1, Register::PC);
+auto isRegisterSP = bind(&verifyExpressionIsRegister, _1, Register::SP);
+auto isRegisterO  = bind(&verifyExpressionIsRegister, _1, Register::O);
+
+
 TEST(ParserTest, ParseInstruction) {
-	list<Statement> statements;
+	list<shared_ptr<Statement>> statements;
 
 	ASSERT_NO_FATAL_FAILURE(runParser("SET A, B\n   add pc  ,\ti\n\njsr X", 3, statements));
 
@@ -72,21 +100,31 @@ TEST(ParserTest, ParseInstruction) {
 	{
 		SCOPED_TRACE("First Statement"); 
 		verifyInstruction(it, Opcode::SET,
-			boost::bind(&verifyArgumentIsRegister, Register::A, _1),
-			boost::bind(&verifyArgumentIsRegister, Register::B, _1));
+			[](shared_ptr<Argument> arg) {
+				verifyArgumentIsExpression(arg, isRegisterA);
+			},
+			[](shared_ptr<Argument> arg) {
+				verifyArgumentIsExpression(arg, isRegisterB);
+			});
 	}
 
 	{
 		SCOPED_TRACE("Second Statement"); 
 		verifyInstruction(it, Opcode::ADD,
-			boost::bind(&verifyArgumentIsRegister, Register::PC, _1),
-			boost::bind(&verifyArgumentIsRegister, Register::I, _1));
+			[](shared_ptr<Argument> arg) {
+				verifyArgumentIsExpression(arg, isRegisterPC);
+			},
+			[](shared_ptr<Argument> arg) {
+				verifyArgumentIsExpression(arg, isRegisterI);
+			});
 	}
 
 	{
 		SCOPED_TRACE("Third Statement"); 
 		verifyInstruction(it, Opcode::JSR,
-			boost::bind(&verifyArgumentIsRegister, Register::X, _1),
+			[](shared_ptr<Argument> arg) {
+				verifyArgumentIsExpression(arg, isRegisterX);
+			},
 			&verifyArgumentIsNull);
 	}
 }
