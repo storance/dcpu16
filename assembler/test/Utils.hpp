cabdb9f1bf0c198dde88e53abbb933a9d3a99bc7
Steven Torance <storance@gmail.com>
2012-04-29 00:28:28 -0400

    Removing commented out code

--- assembler/test/Utils.hpp
+++ assembler/test/Utils.hpp
@@ -2,22 +2,9 @@
 
 #include <string>
 #include <cstdint>
-#include <functional>
 
 #include "../Lexer.hpp"
 #include "../Parser.hpp"
-#include "../ast/Expression.hpp"
-
-/*typedef std::function<void (dcpu::ExpressionPtr&)> ExpressionFunc;
-
-ExpressionFunc assertIsBinaryOperation(dcpu::ast::BinaryOperator, ExpressionFunc, ExpressionFunc);
-ExpressionFunc assertIsUnaryOperation(dcpu::ast::UnaryOperator, ExpressionFunc);
-ExpressionFunc assertIsLabelRef(const std::string&);
-ExpressionFunc assertIsRegister(dcpu::ast::Register);
-ExpressionFunc assertIsLiteral(std::uint32_t);
-ExpressionFunc assertIsEvaluatedRegister(dcpu::ast::Register, bool, std::int32_t);
-ExpressionFunc assertIsEvaluatedRegister(dcpu::ast::Register);
-ExpressionFunc assertIsEvaluatedLiteral(std::int32_t);*/
 
 void runLexer(const std::string&, int, std::shared_ptr<dcpu::lexer::Lexer>&);
 void runParser(const std::string&, int, std::shared_ptr<dcpu::parser::Parser>&);
